38e5f56a031e9ee2b94f80ad82730f20862bf0c7
Fabio De Simoni <fabiodesimoni@Fabios-MacBook-Pro.local>
2018-11-22 16:53:57 +0000

    Initial commit

---
+++ boards/tests/test_view_home.py
@@ -0,0 +1,38 @@
+# Create your tests here.
+
+from django.urls import reverse
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import resolve
+from ..views import home, board_topics, new_topic
+from ..models import Board, Topic, Post
+from ..forms import NewTopicForm
+
+# class HomeTests(TestCase):
+#     def test_home_view_status_code(self):
+#         url = reverse('home')
+#         response = self.client.get(url)
+#         self.assertEquals(response.status_code, 200)
+#
+#
+#     def test_home_url_resolves_home_view(self):
+#         view = resolve('/')
+#         self.assertEquals(view.func, home)
+
+class HomeTests(TestCase):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board.')
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_home_view_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func, home)
+
+    def test_home_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        print('Return: ' + 'href="{0}"'.format(board_topics_url))
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
